ee5d1626c82eeeb60795bf71813f409e37b86788
Mateusz Stadnik <matgla@live.com>
2019-08-13 18:47:05 +0200

    fixes in hal for usart

---
+++ src/x86/common/usart/x86_common_usart.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <thread>
+#include <functional>
+
+#include <boost/asio/serial_port.hpp>
+
+#include "x86/simulator/detail/tcp_server.hpp"
+
+#include "hal/interfaces/usart.hpp"
+
+namespace hal
+{
+namespace x86
+{
+namespace common
+{
+namespace usart
+{
+
+class UsartDriver : public hal::interfaces::UsartInterface
+{
+public:
+    UsartDriver(const std::string_view& device)
+        : io_(new boost::asio::io_service())
+        , port_(new boost::asio::serial_port(*io_))
+        , device_(device)
+    {
+    }
+
+    UsartDriver(UsartDriver&& usart)
+        : io_(std::move(usart.io_))
+        , port_(std::move(usart.port_))
+        , thread_(std::move(usart.thread_))
+        , device_(usart.device_)
+    {
+
+    }
+
+    ~UsartDriver()
+    {
+        if (port_ != nullptr && port_->is_open())
+        {
+            port_->cancel();
+            port_->close();
+
+            io_->stop();
+            thread_.join();
+        }
+    }
+
+    void init(uint32_t baudrate) override
+    {
+        port_->open(std::string(device_));
+        port_->set_option(boost::asio::serial_port_base::baud_rate(baudrate));
+
+        startListening();
+
+        thread_ = std::thread([this] {io_->run();});
+    }
+
+    void setBaudrate(uint32_t baudrate) override
+    {
+        (void)(baudrate);
+    }
+
+    void write(const StreamType& data) override
+    {
+        for (const auto byte : data)
+        {
+            write(byte);
+        }
+    }
+
+private:
+    void write(const uint8_t byte)
+    {
+        uint8_t buffer[1];
+        buffer[0] = byte;
+        boost::system::error_code ec;
+        port_->write_some(boost::asio::buffer(buffer, 1), ec);
+    }
+
+    void startListening()
+    {
+        if (port_ == nullptr || !port_->is_open())
+        {
+            return;
+        }
+        port_->async_read_some(
+            boost::asio::buffer(buffer_, 256),
+            [this](boost::system::error_code ec, std::size_t bytes_transferred)
+            {
+                if (!bytes_transferred)
+                {
+                    startListening();
+                    return;
+                }
+
+                if (ec)
+                {
+                    startListening();
+                    return;
+                }
+
+                on_data_signal_.emit(gsl::make_span(buffer_, bytes_transferred));
+
+                startListening();
+            }
+        );
+    }
+
+    std::unique_ptr<boost::asio::io_service> io_;
+    std::unique_ptr<boost::asio::serial_port> port_;
+    uint8_t buffer_[256];
+    std::thread thread_;
+    std::string_view device_;
+};
+
+
+} // namespace usart
+} // namespace common
+} // namespace x86
+} // namespace hal
