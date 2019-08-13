b5a9d9ad39901fdca958fe56ed5fa83d6c9a2075
Mateusz Stadnik <matgla@live.com>
2019-08-13 18:53:39 +0200

    fixes for x86 usart

--- boards/x86/linux/usart_config.hpp
+++ boards/x86/linux/usart_config.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "x86/simulator/usart/x86_simulator_usart.hpp"
+#include "x86/common/usart/x86_simulator_usart.hpp"
 #include "boards/containers/usart_container.hpp"
 #include "boards/containers/drivers_pack.hpp"
 #include "boards/containers/driver_holder.hpp"
@@ -14,8 +14,8 @@ class Usart0;
 class Usart1;
 
 static auto UsartContainer = hal::boards::make_usart_container(hal::boards::DriversPack(
-    hal::boards::make_holder<Usart0>(x86::simulator::usart::UsartDriver("/dev/ttyVS0")),
-    hal::boards::make_holder<Usart1>(x86::simulator::usart::UsartDriver("/dev/ttyVS1"))
+    hal::boards::make_holder<Usart0>(x86::common::usart::UsartDriver("/dev/ttyVS0")),
+    hal::boards::make_holder<Usart1>(x86::common::usart::UsartDriver("/dev/ttyVS1"))
 ));
 
 } // namespace board
