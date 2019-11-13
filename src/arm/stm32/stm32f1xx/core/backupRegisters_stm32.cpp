f21d463b13f322c16421207a8e76f7dab423ad44
Mateusz Stadnik <matgla@live.com>
2019-11-13 19:35:23 +0100

    backup registers implementation

--- src/arm/stm32/stm32f1xx/core/backupRegisters_stm32.cpp
+++ src/arm/stm32/stm32f1xx/core/backupRegisters_stm32.cpp
@@ -1,80 +1,78 @@
-// #include "hal/core/backupRegisters.hpp"
-// 
-// #include <stm32f10x.h>
-// 
-// 
-// #define STARTUP_REGISTER 1 
-// #define STARTUP_MAGIC_COOKIE 0xBEEF
-// 
-// #define PWR_OFFSET (PWR_BASE - PERIPH_BASE)
-// 
-// #define CR_OFFSET (PWR_OFFSET + 0x00)
-// #define DBP_BitNumber 0x08
-// #define CR_DBP_BB  (PERIPH_BB_BASE + (CR_OFFSET * 32) + (DBP_BitNumber * 4))
-// 
-// #define RCC_OFFSET (RCC_BASE - PERIPH_BASE)
-// #define BDCR_OFFSET (RCC_OFFSET + 0x20)
-// #define BDRST_BitNumber 0x10
-// #define BDCR_BDRST_BB (PERIPH_BB_BASE + (BDCR_OFFSET * 32) + (BDRST_BitNumber * 4))
-// 
-// namespace 
-// {
-//     constexpr uint16_t numberToRegister(const uint16_t reg)
-//     {
-//         switch (reg)
-//         {
-//             case 1: return 0x0008;
-//             case 2: return 0x000c;
-//             case 3: return 0x0010;
-//         }
-//         return 0x0004; 
-//     }
-// }
-// 
-// namespace hal
-// {
-// namespace core
-// {
-// 
-// constexpr bool BackupRegisters::isFirstStartup()
-// {
-//     return read(STARTUP_REGISTER) != STARTUP_MAGIC_COOKIE;
-// }
-// 
-// constexpr void BackupRegisters::write(u16 registerNumber, u16 value)
-// {
-//     /* enable backup register power */
-//     *reinterpret_cast<uint32_t*>(CR_DBP_BB) = 1;
-//     const uint32_t address = 0x40006c00 + numberToRegister(registerNumber);
-//     *reinterpret_cast<uint32_t*>(address) = value;
-//     *reinterpret_cast<uint32_t*>(CR_DBP_BB) = 0;
-// }
-// 
-// constexpr uint32_t BackupRegisters::read(uint16_t registerNumber)
-// {
-//     const uint32_t address = 0x40006c00 + numberToRegister(registerNumber);
-//     return *reinterpret_cast<uint16_t*>(address);
-// }
-// 
-// constexpr void BackupRegisters::startupDone()
-// {
-//     if (isFirstStartup())
-//     {
-//         write(STARTUP_REGISTER, STARTUP_MAGIC_COOKIE);
-//     }
-// }
-// 
-// void BackupRegisters::init()
-// {
-//     RCC->APB1ENR |= 0x08000000; // bkp 
-//     RCC->APB1ENR |= 0x10000000; // pwr
-//     if (isFirstStartup())
-//     {
-//        *(uint32_t*) BDCR_BDRST_BB = 1;
-//        *(uint32_t*) BDCR_BDRST_BB = 0;
-//     }
-// }
-// 
-// 
-// } // namespace hal
-// } // namespace core
+#include "hal/core/backupRegisters.hpp"
+
+#include <eul/memory_ptr.hpp>
+
+#include <stm32f10x.h>
+
+namespace
+{
+    constexpr uint16_t register_number_to_address(const uint16_t register_number)
+    {
+        return (register_number + 1) * 4;
+    }
+
+    volatile uint32_t& memory(std::size_t address)
+    {
+        return *reinterpret_cast<uint32_t*>(address);
+    }
+}
+
+namespace hal 
+{
+namespace core 
+{
+
+constexpr uint16_t startup_register = 0;
+constexpr uint16_t startup_magic_cookie = 0xbeef;
+
+constexpr uint32_t pwr_offset = (PWR_BASE - PERIPH_BASE);
+constexpr uint32_t cr_offset = pwr_offset + 0x00;
+constexpr uint32_t dbp_bit_number = 0x08;
+constexpr uint32_t cr_dbp_bb = PERIPH_BB_BASE + cr_offset * 32 + dbp_bit_number * 4;
+constexpr uint32_t backup_register_base_address = 0x40006c00;
+
+constexpr uint32_t rcc_offset = (RCC_BASE - PERIPH_BASE);
+constexpr uint32_t bdcr_offset = (rcc_offset + 0x20);
+constexpr uint32_t bdrst_bit_number = 0x10;
+constexpr uint32_t bdcr_bdrst_bb = (PERIPH_BB_BASE + (bdcr_offset * 32) + (bdrst_bit_number * 4));
+
+constexpr uint32_t enable_bkp_rcc = 0x08000000;
+constexpr uint32_t enable_pwr_rcc = 0x10000000;
+
+uint32_t BackupRegisters::read(uint16_t register_number)
+{
+    uint32_t address = backup_register_base_address + register_number_to_address(register_number);
+    return memory(address);
+}
+
+void BackupRegisters::write(const uint16_t register_number, uint16_t value)
+{
+    /* enable backup register power */
+    static constexpr eul::memory_ptr<uint32_t> power_backup_access_register(cr_dbp_bb);
+    *power_backup_access_register = 1;
+    const uint32_t address = backup_register_base_address + register_number_to_address(register_number);
+    const eul::memory_ptr<uint32_t> backup_register(address);
+    *backup_register = value;
+    *power_backup_access_register = 0;
+} 
+
+void BackupRegisters::init()
+{
+    RCC->APB1ENR |= enable_bkp_rcc; 
+    RCC->APB1ENR |= enable_pwr_rcc;
+    if (read(startup_register) == startup_magic_cookie)
+    {
+        reset();
+        write(startup_register, startup_magic_cookie);
+    }
+}
+
+void BackupRegisters::reset()
+{
+     memory(bdcr_bdrst_bb) = 1;
+     memory(bdcr_bdrst_bb) = 0;
+}
+
+} // namespace core
+} // namespace hal
+
