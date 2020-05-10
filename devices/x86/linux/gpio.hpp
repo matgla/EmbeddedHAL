8102ca1aa0649d270523299fad8b1afbdd713182
Mateusz Stadnik <matgla@live.com>
2020-05-10 14:48:46 +0200

    gpio emulation on x86

---
+++ devices/x86/linux/gpio.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "x86/common/gpio/x86_common_gpio.hpp"
+
+namespace hal
+{
+namespace gpio
+{
+using DigitalInputOutputPin = hal::gpio::X86CommonGpio;
+
+} // namespace gpio
+} // namespace hal
---
+++ src/x86/common/gpio/x86_common_gpio.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "hal/gpio/digital_input_output_pin.hpp"
+
+#include <eul/utils/unused.hpp>
+
+#include <atomic>
+
+namespace hal
+{
+namespace gpio
+{
+
+class X86CommonGpio
+{
+public:
+    void init(hal::gpio::Input input)
+    {
+        UNUSED1(input);
+    }
+
+    void setHigh()
+    {
+        is_high_ = true;
+    }
+
+    void setLow()
+    {
+        is_high_ = false;
+    }
+
+    bool read()
+    {
+        return is_high_;
+    }
+
+    std::atomic<bool> is_high_ = false;
+};
+
+template <int id>
+class X86ButtonFactory : public X86CommonGpio
+{
+public:
+    static X86CommonGpio& get()
+    {
+        static X86ButtonFactory<id> a;
+        return a;
+    }
+private:
+    X86ButtonFactory() = default;
+};
+
+
+} // namespace gpio
+} // namespace hal
