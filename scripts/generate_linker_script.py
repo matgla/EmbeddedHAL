5da5325dc34c6973e6aab5209319b6f889845d71
Mateusz Stadnik <matgla@live.com>
2020-09-14 21:28:11 +0200

    python generator for linker scripts

---
+++ scripts/generate_linker_script.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+
+# This file is part of Embedded HAL project.
+# Copyright (C) 2020 Mateusz Stadnik
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+import os
+import json
+import sys
+import re
+
+from jinja2 import Template, Environment, FileSystemLoader
+
+
+parser = argparse.ArgumentParser(description = "Linker script generator for HAL")
+parser.add_argument("-i", "--input", dest="input", action="store", help="Path to configuration file file", required=True)
+parser.add_argument("-o", "--output", dest="output", action="store", help="Path to output file", required=True)
+parser.add_argument("-s", "--sections", dest="options", action="store", help="Additional sections in format: <name>:<access(r/w/x)>:address:length, separated by comma (',')")
+
+args, rest = parser.parse_known_args()
+
+size_units = {
+    "B": 1,
+    "KB": 10**3,
+    "K": 10**3,
+    "MB": 10**6,
+    "M": 10**6,
+    "GB": 10**9,
+    "G": 10**9,
+    "TB": 10**12,
+    "T": 10**12
+}
+
+import re
+
+# based on https://stackoverflow.com/a/60708339
+units = {
+    "B": 1,
+    "K": 2**10,
+    "KB": 2**10,
+    "M": 2**20,
+    "MB": 2**20,
+    "G": 2**30,
+    "GB": 2**30,
+    "T": 2**40,
+    "TB": 2**40
+}
+
+def parse_size(size):
+    size = size.upper()
+    print("size before: ", size)
+    if not any(i in size for i in 'TGMKB'):
+        if size.find("X"):
+            return int(size, base=16)
+        else:
+            return int(size)
+    print("Size: ", size)
+    if not re.match(r' ', size):
+        print("Inserting space")
+        size = re.sub(r'([KMGT]B?)', r' \1', size)
+    print (size.split())
+    number, unit = [string.strip() for string in size.split()]
+
+    return int(float(number)*units[unit])
+
+def to_kilobytes(size):
+    return str(int(size/1024)) + "K"
+
+def main():
+
+    config = None
+    with open(args.input) as config_file:
+        config = json.loads(config_file.read())
+
+    if config is None:
+        sys.exit(-1)
+
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    template_loader = FileSystemLoader(current_path)
+    env = Environment(loader = template_loader)
+    t = env.get_template("linker_script.ld.template")
+
+    sections = []
+    for section_key in config["memory"]["sections"]:
+        value = config["memory"]["sections"][section_key]
+        # if parts exists then section is splitted
+        subsections = []
+        section_size = parse_size(value["size"])
+        if "parts" in value:
+            section_start_address = parse_size(value["address"])
+            section_size = parse_size(value["size"])
+            section_end_address = section_start_address + section_size
+            print("start: ", hex(section_start_address))
+            print("start: ", hex(section_end_address))
+            additional_size = 0
+            previous_address = section_end_address
+
+            for part_value in value["parts"]:
+                print ("part: ", part_value)
+                subsection_size = parse_size(part_value["size"])
+                subsection_address = previous_address - subsection_size
+                additional_size = additional_size + subsection_size
+                previous_address = subsection_address
+                subsections.append({
+                    "name": part_value["name"],
+                    "access": value["access"],
+                    "address": hex(subsection_address),
+                    "length": to_kilobytes(subsection_size)
+                })
+                if additional_size > section_size:
+                    raise RuntimeError("Subsections size higher that section (" + to_kilobytes(additional_size) + " > " + to_kilobytes(section_size) + ")")
+
+            section_size = section_size - additional_size
+        sections.append({
+            "name": section_key,
+            "access": value["access"],
+            "address": value["address"],
+            "length": to_kilobytes(section_size)
+        })
+
+        sections = sections + subsections
+
+    rendered = t.render(
+        sections = sections,
+        heap = hex(parse_size(config["memory"]["heap"]["size"])),
+        stack = hex(parse_size(config["memory"]["stack"]["size"]))
+    )
+
+    print(rendered)
+    with open(args.output + "/linker_script.ld", "w") as output:
+        output.write(rendered)
+
+main()
---
+++ scripts/generate_soc.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+# This file is part of Embedded HAL project.
+# Copyright (C) 2020 Mateusz Stadnik
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+import sys
+import os
+import json
+import collections
+
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description = "CMake configuration generator based on KConfig")
+parser.add_argument("-s", "--soc", dest="soc", action="store", help="SOC name", required=True)
+parser.add_argument("-i", "--input", dest="input_directory", action="store", help="Path to input directory", required=True)
+parser.add_argument("-o", "--output", dest="output_directory", action="store", help="Path to output directory", required=True)
+parser.add_argument("-u", "--user_configs", dest="user_directory", action="store", help="Path to user provided configs")
+
+args, rest = parser.parse_known_args()
+
+processed_files = []
+
+def get_all_configs(soc, input_directory, user_directory):
+    next_config = soc
+    configs = []
+
+    soc_path = None
+    base_config = None
+
+    for path in Path(args.input_directory).rglob(soc + ".json"):
+        soc_path = path
+        processed_files.append(soc_path)
+        break
+
+    if not soc_path is None:
+        with open(soc_path) as config_file:
+            base_config = json.loads(config_file.read())
+
+    soc_path = None
+    if not user_directory is None:
+        print ("Searching in " + args.user_directory + " :" + soc + ".json")
+        for path in Path(args.user_directory).rglob(soc + ".json"):
+            soc_path = path
+            processed_files.append(soc_path)
+            break
+
+    print ("User path: ", soc_path)
+    if not soc_path is None:
+        with open(soc_path) as config_file:
+            if base_config is None:
+                base_config = json.loads(config_file.read())
+            else:
+                print("Merge: ")
+                merge(base_config, json.loads(config_file.read()))
+
+    if base_config is None:
+        sys.exit(-1)
+
+    configs.append(base_config)
+    while not next_config is None:
+        if "parent" in configs[-1]:
+            next_config = None
+            new_config = None
+            for path in Path(input_directory).rglob(configs[-1]["parent"] + ".json"):
+                next_config = path
+                processed_files.append(next_config)
+                break
+
+            if not next_config is None:
+                with open(next_config) as config_file:
+                    new_config = json.loads(config_file.read())
+
+            if not user_directory is None:
+                for path in Path(user_directory).rglob(configs[-1]["parent"] + ".json"):
+                    next_config = path
+                    processed_files.append(next_config)
+                    break
+
+                if not next_config is None:
+                    with open(next_config) as config_file:
+                        part = json.loads(config_file.read())
+                        if new_config is None:
+                            new_config = part
+                        else:
+                            print ("Merge 2")
+                            print ("1: ", new_config)
+                            print ("2: ", part)
+                            new_config = merge(new_config, part)
+
+            if new_config == None:
+                print("Cannot find file: " + input_directory + "/../" + configs[-1]["parent"] + ".json")
+                sys.exit(-1)
+            print("Append: ", new_config)
+            if "parent" in new_config:
+                del new_config["parent"]
+            print("Appenfing: ", new_config)
+            configs.append(new_config)
+        else:
+            next_config = None
+
+    return configs
+
+def merge(old, new):
+    for k in new:
+        if k in old:
+            if isinstance(old[k], dict):
+                merge(old[k], new[k])
+            else:
+                old[k] = new[k]
+        else:
+            old[k] = new[k]
+
+    return old
+
+def write_node(node, prefix, file):
+    print("node: ", node)
+    for key in node:
+        if prefix == "":
+            pref = key
+        else:
+            pref = prefix + "_" + key
+        print ("key: ", key)
+        if not isinstance(node[key], dict):
+            file.write("set (" + pref + " " + str(node[key]) + ")\n")
+        else:
+            write_node(node[key], pref, file)
+
+def main():
+    print("Searching: " + args.soc)
+
+    if not os.path.exists(args.output_directory):
+        os.makedirs(args.output_directory)
+
+    configs = get_all_configs(args.soc, args.input_directory, args.user_directory)
+    config = {}
+    for c in configs:
+        merge(config, c)
+
+    with open(args.output_directory + "/soc_config.json", "w") as file:
+        json.dump(config, file)
+    cmake_soc_file = args.output_directory + "/soc_config.cmake"
+    with open(cmake_soc_file, "w") as soc_config:
+        write_node(config, "", soc_config)
+        for file in processed_files:
+            soc_config.write("set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS " + str(file) + ")\n")
+
+main()
