d0f298e92ae4efc5e1b6094eaea548ad377c1412
Mateusz Stadnik <matgla@live.com>
2020-10-02 23:13:59 +0200

    data and instruction cache breaks my vga

--- socs/arm/stm32/stm32f4xx/stm32f401ceu6/stm32f4xx_hal_conf.h
+++ socs/arm/stm32/stm32f4xx/stm32f401ceu6/stm32f4xx_hal_conf.h
@@ -150,8 +150,8 @@
 #define  TICK_INT_PRIORITY            (0x0FU) /*!< tick interrupt priority */
 #define  USE_RTOS                     0U
 #define  PREFETCH_ENABLE              1U
-#define  INSTRUCTION_CACHE_ENABLE     1U
-#define  DATA_CACHE_ENABLE            1U
+#define  INSTRUCTION_CACHE_ENABLE     0U
+#define  DATA_CACHE_ENABLE            0U
 
 #define  USE_HAL_ADC_REGISTER_CALLBACKS         0U /* ADC register callback disabled       */
 #define  USE_HAL_CAN_REGISTER_CALLBACKS         0U /* CAN register callback disabled       */
